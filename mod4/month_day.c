c392779f5026ed3a2803b75778e6d7785d4adb7e
Chase May <chasemay@mail.weber.edu>
2019-01-31 09:43:33 -0700

    Add constants for months and conditional to validate days of months

--- mod4/month_day.c
+++ mod4/month_day.c
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  01/31/2019 09:16:52 AM
  *       Revision:  none
- *       Compiler:  gcc
+ *       Compiler:  gcc month_day.c -o month_day.out
  *
  *         Author:  Chase May (), chasemay@mail.weber.edu
  *   Organization:  WSU
@@ -19,18 +19,70 @@
 
 // Constants
 
+#define JANUARY   1
+#define FEBRUARY  2
+#define MARCH     3
+#define APRIL     4
+#define MAY       5
+#define JUNE      6
+#define JULY      7
+#define AUGUST    8
+#define SEPTEMBER 9
+#define OCTOBER   10
+#define NOVEMBER  11
+#define DECEMBER  12
+
 // Function Prototypes
 
 // Main Function
 int main()
 {
     int month, day;
+
     printf("Please enter the month (in digits, e.g. '12' for December): ");
     scanf("%d", &month);
+
+    if (month < 1 || month > 12) {
+        printf("Invalid month.\n");
+        return 1;
+    }
+
     printf("Please enter the day (in digits, e.g. '27'): ");
     scanf("%d", &day);
+    
+    switch (month) {
+        // Months with 31 days
+        case JANUARY:
+        case MARCH:
+        case MAY:
+        case JULY:
+        case AUGUST:
+        case OCTOBER:
+        case DECEMBER:
+            if (day > 31 || day < 1) {
+                printf("Invalid day.\n");
+                return 1;
+            }
+            break;
+        // Months with 30 days
+        case APRIL:
+        case JUNE:
+        case SEPTEMBER:
+        case NOVEMBER:
+            if (day > 30 || day < 1) {
+                printf("Invalid day.\n");
+                return 1;
+            }
+            break;
+        case FEBRUARY:
+            if (day > 28 || day < 1) {
+                printf("Invalid day.\n");
+                return 1;
+            }
+            break;
+    }
 
-    printf("The month is %d and the day is %d", month, day);
+    printf("The month is %d and the day is %d\n.", month, day);
     return 0;
 }
 // Function Definitions
