a46f436f71d9f4c4058590d8cc06ae8384ed9c8f
Chase May <chasemay@mail.weber.edu>
2019-02-05 08:52:12 -0700

    Update to initialize all variables to 0

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -28,19 +28,21 @@ int main()
 {
     // Task 1: Calculate the average of user input values
     int count = 0;
-    float grade, sum, avg;
+    float grade = 0;
+    float total = 0;
+    float avg = 0;
 
     while (count < MAX_COUNT) {
         printf("\nEnter %d grade (0.0-100.0): ", count + 1);
         scanf("%f", &grade);
 
-        sum += grade;
+        total += grade;
 
         // Update test condition
         count++;
     }
 
-    avg = sum / count;
+    avg = total / count;
 
     printf("The average of the grades is %6.2f.\n", avg);
     printf("\nBye\n");
