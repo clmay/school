4e114b56b40ec4aa95634588e94cf1f00c88141b
Chase May <chasemay@mail.weber.edu>
2019-02-05 08:49:57 -0700

    Add firstwhile.c

---
+++ mod5/firstwhile.c
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  firstwhile.c
+ *
+ *    Description:  Learn `while` loops
+ *
+ *        Version:  1.0
+ *        Created:  02/05/2019 08:36:45 AM
+ *       Revision:  none
+ *       Compiler:  gcc firstwhile.c -o firstwhile.out
+ *
+ *         Author:  Chase May (), chasemay@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+#define MAX_COUNT 4
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    // Task 1: Calculate the average of user input values
+    int count = 0;
+    float grade, sum, avg;
+
+    while (count < MAX_COUNT) {
+        printf("\nEnter %d grade (0.0-100.0): ", count + 1);
+        scanf("%f", &grade);
+
+        sum += grade;
+
+        // Update test condition
+        count++;
+    }
+
+    avg = sum / count;
+
+    printf("The average of the grades is %6.2f.\n", avg);
+    printf("\nBye\n");
+
+    return 0;
+}
+// Function Definitions
+
+
