cb25c450448a0982c05b1c064c33311f9b373e44
Chase May <chaseloganmay@gmail.com>
2019-02-04 23:41:12 -0700

    Complete GUI & graphics assignments from unit three

--- unit03/AboutMe.java
+++ unit03/AboutMe.java
@@ -6,21 +6,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class AboutMe extends JPanel {
+
+  String aboutText = "<html><body>Hello! My name is Chase May. Welcome!";
+  String educationText = "<html><body>I am currently a student of computer science at Weber State University. In the past, I have studied linguistics and Native American languages in Oregon.</body></html>";
+  String projectsText = "<html><body>You can view my current projects by visiting my <a href=\"https://github.com/chsm\">GitHub profile</a>. Recent projects include Pac-Man graphics, a palindrome tester, and others.</body></html>";
+  String hobbiesText = "<html><body>Some of my favorite hobbies are cooking, traveling, learning new (human) languages, and learning new computer languages.</body></html>";
+
   private JFrame window = new JFrame("About Me");
-  private JLabel lblText = new JLabel("<html><body>Hello! My name is Chase May. Please click the buttons below to learn more about me.</body></html>");
+  private JButton btnAbout = new JButton("About me");
   private JButton btnEducation = new JButton("Education");
+  private JButton btnProjects = new JButton("Projects");
   private JButton btnHobbies = new JButton("Hobbies");
+  private JLabel lblText = new JLabel(aboutText);
 
   public AboutMe() {
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setSize(400, 400);
+    window.setSize(400, 350);
     setLayout(null);
 
-    lblText.setBounds(20, 20, 360, 200);
-    btnEducation.setBounds(20, 200, 200, 20);
-    btnHobbies.setBounds(220, 200, 100, 20);
+    lblText.setBounds(30, 20, 360, 180);
+    btnAbout.setBounds(30, 200, 150, 20);
+    btnEducation.setBounds(220, 200, 150, 20);
+    btnProjects.setBounds(30, 240, 150, 20);
+    btnHobbies.setBounds(220, 240, 150, 20);
     this.add(lblText);
+    this.add(btnAbout);
     this.add(btnEducation);
+    this.add(btnProjects);
     this.add(btnHobbies);
 
     window.add(this);
@@ -28,13 +40,29 @@ class AboutMe extends JPanel {
     window.setVisible(true);
 
     // Listeners
-
-    btnEducation.addActionListener(new ActionListener() {
+    btnAbout.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        lblText.setText("I am currently a student of computer science at Weber State University. In the past, I have studied linguistics and Native American languages in Oregon.");
+        lblText.setText(aboutText);
       }
     });
 
+    btnEducation.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        lblText.setText(educationText);
+      }
+    });
+
+    btnProjects.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        lblText.setText(projectsText);
+      }
+    });
+
+    btnHobbies.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        lblText.setText(hobbiesText);
+      }
+    });
   }
 
   public static void main(String[] args) {
