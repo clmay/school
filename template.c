e23cc9573eb13a0d723e5b1d4cda7d26294d6980
Chase May <chaseloganmay@gmail.com>
2019-02-12 09:59:48 -0700

    Add template.c

---
+++ template.c
@@ -0,0 +1,30 @@
+ /*
+  * =====================================================================================
+  *
+  *       Filename:
+  *
+  *    Description:
+  *
+  *        Version:  1.0
+  *        Created:  02/12/2019 08:39:17 AM
+  *       Revision:  none
+  *       Compiler:
+  *
+  *         Author:  Chase May (), chasemay@mail.weber.edu
+  *   Organization:  WSU
+  *
+  * =====================================================================================
+  */
+
+ #include <stdio.h>
+
+ // Constants
+
+ // Function Prototypes
+
+ // Main Function
+ int main() {
+
+   return 0;
+ }
+ // Function Definitions
