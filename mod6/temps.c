e25148587dbd20bf1bf8465b73a3c2eb59f99e9d
Chase May <chasemay@mail.weber.edu>
2019-02-12 08:45:26 -0700

    Add temps.c

---
+++ mod6/temps.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  temps.c
+ *
+ *    Description:  Temperature analysis
+ *
+ *        Version:  1.0
+ *        Created:  02/12/2019 08:39:17 AM
+ *       Revision:  none
+ *       Compiler:  gcc temps.c -o temps.out
+ *
+ *         Author:  Chase May (), chasemay@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+#define NUM 4
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    double ogden_highs[NUM];
+
+    ogden_highs[0] = 40;
+    ogden_highs[1] = 40;
+    ogden_highs[2] = 40;
+    ogden_highs[3] = 40;
+
+
+    return 0;
+}
+// Function Definitions
+
+
