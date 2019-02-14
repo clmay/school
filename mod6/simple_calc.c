9bd907f698fd567b2b1c5ca77bdbc6adf57454d4
Chase May <chaseloganmay@gmail.com>
2019-02-14 10:18:54 -0700

    Add loop to simple_calc.c

--- mod6/simple_calc.c
+++ mod6/simple_calc.c
@@ -17,35 +17,57 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <stdlib.h>   // atoi()
 
 // Constants
+
+#define EXIT      0
+#define ADD       1
+#define SUBTRACT  2
+#define MULTIPLY  3
 
 // Function Prototypes
 
 // Main Function
 int main(int argc, char* argv[])
 {
-    int choice;
-    double sum;
-    double diff;
-    double prod;
+    int choice = 0;
+    int num1, num2;
 
     if (argc != 3) {
-        printf("Missing input:\n");
+        printf("\n  Missing input:\n");
         printf("Usage: %s <first number> <second number>\n", argv[0]);
         return 1;
     }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
-    printf("Operation?\n");
-    printf("\t1) Addition\n");
-    printf("\t1) Subtraction\n");
-    printf("\t1) Multiplication\n");
-    scanf("%d", &choice);
+    do {
+      printf("\nOperation?\n");
+      printf("  0) Quit\n");
+      printf("  1) Addition\n");
+      printf("  2) Subtraction\n");
+      printf("  3) Multiplication\n");
+      scanf("%d", &choice);
 
-
+      switch (choice) {
+        case EXIT:
+          printf("\nGoodbye!\n");
+          break;
+        case ADD:
+          printf("\n%d + %d = %d\n", num1, num2, num1 + num2);
+          break;
+        case SUBTRACT:
+          printf("\n%d - %d = %d\n", num1, num2, num1 - num2);
+          break;
+        case MULTIPLY:
+          printf("\n%d * %d = %d\n", num1, num2, num1 * num2);
+          break;
+        default:
+          printf("\nUnsupported function.\n");
+      }
+    } while (choice != EXIT);
 
     return 0;
 }
 // Function Definitions
-
-
