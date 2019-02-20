3c10ed55a7719990a74140cd25aef88c2b441a3f
Chase May <chaseloganmay@gmail.com>
2019-02-20 11:29:41 -0700

    Update game logic, make code more compact

--- unit05/Pig.java
+++ unit05/Pig.java
@@ -1,5 +1,4 @@
 import java.awt.event.*;
-
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,11 +6,9 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 class Pig extends JPanel {
-  // Create the window
-  private JFrame window = new JFrame("The Game of Pig");
-
-  // Initialize components
-  private JLabel dieLabel = new JLabel("Current die value.");
+  private JFrame window = new JFrame("The Game of Pig");        // Create the window
+  private JLabel dieLabel = new JLabel("Current die value.");   // Initialize components
+  private JLabel counterLabel = new JLabel("Number of rolls.");
   private JLabel userScoreLabel = new JLabel("Player Score.");
   private JLabel aiScoreLabel = new JLabel("Computer Score.");
   private JButton rollButton = new JButton("Roll");
@@ -22,61 +19,69 @@ class Pig extends JPanel {
   private PigPlayer ai = new PigPlayer();
 
   public Pig() {
-    // Initialize window
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setSize(300, 240);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Initialize window
+    window.setSize(300, 300);
     setLayout(null);
-
-    // Position components
-    // setBounds(x, y, width, height) (https://docs.oracle.com/javase/8/docs/api/java/awt/Component.html#setBounds-int-int-int-int-)
-    dieLabel.setBounds(30, 20, 270, 20);
-    userScoreLabel.setBounds(30, 60, 270, 20);
+    dieLabel.setBounds(30, 20, 100, 20);        // Position components
+    counterLabel.setBounds(140, 20, 200, 20);   // setBounds(x, y, width, height)
+    userScoreLabel.setBounds(30, 60, 270, 20);  // (https://docs.oracle.com/javase/8/docs/api/java/awt/Component.html#setBounds-int-int-int-int-)
     aiScoreLabel.setBounds(30, 100, 270, 20);
     rollButton.setBounds(20, 140, 100, 30);
     stopButton.setBounds(140, 140, 100, 30);
-
-    // Add components to `Pig` object
     this.add(dieLabel);
+    this.add(counterLabel);
     this.add(userScoreLabel);
     this.add(aiScoreLabel);
     this.add(rollButton);
     this.add(stopButton);
-
-    // Add `Pig` to window
-    window.add(this);
-
-    // Render window
-    window.setVisible(true);
+    window.add(this);         // Add `Pig` to window
+    window.setVisible(true);  // Render window
+    update();                 // Init game variables
 
     rollButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         user.rollDie();
-        update();
         if (user.playerWon()) {
           JOptionPane.showMessageDialog(null, "You win!");
+          return;
         }
+        update();
       }
     });
-
     stopButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        user.endRound();
-        if (user.playerWon()) {
-          JOptionPane.showMessageDialog(null, "You win!");
+        if (user.isPlayerTurn()) {
+          user.rollDie();
+          if (user.playerWon()) {
+            JOptionPane.showMessageDialog(null, "You win!");
+          }
+          user.endRound();
+          update();
+        } else {
+          aiRound();
         }
       }
     });
-
-  } // end Pig()
-
+  } // end Pig constructor
+  public void aiRound() {
+    do {
+      ai.rollDie();
+      if (ai.playerWon()) {
+        JOptionPane.showMessageDialog(null, "Computer won!");
+        break;
+      }
+    } while (ai.getRoundScore() < 10);
+    ai.endRound();
+    update();
+  }
   public void update() {
     dieLabel.setText("Die: " + user.getDieValue());
+    counterLabel.setText("Number of rolls: " + user.getCounter());
     userScoreLabel.setText(
       "Player 1 Round Score: " + user.getRoundScore() + "\nTotal Score: " + user.getTotalScore());
     aiScoreLabel.setText(
       "Computer Round Score: " + ai.getRoundScore() + "\nTotal Score: " + ai.getTotalScore());
   }
-
   public static void main(String[] args) {
     new Pig();
   }
--- unit05/PigPlayer.java
+++ unit05/PigPlayer.java
@@ -1,56 +1,71 @@
 import java.util.Random;
 
 class PigPlayer {
-
   // Instance variables
   private static Random gen = new Random();
+  private int winningScore; // the score needed to win the game
+  private boolean playerTurn; // whether it is the current player's turn
+  private int counter; // counts the number of rolls
+  private int dieValue; // the current die value
+  private int roundScore; // points won this round
+  private int totalScore; // total score
 
-  private int dieValue; // class' die variable
-  private int roundScore;
-  private int totalScore;
-
-  // Constructor
-  public PigPlayer() {
+  public PigPlayer() {  // Constructor
+    winningScore = 100;
+    counter = 0;
     dieValue = 1;
     roundScore = 0;
     totalScore = 0;
   }
-
+  public boolean isPlayerTurn() {
+    return playerTurn;
+  }
+  public void togglePlayerTurn() {
+    playerTurn = !(playerTurn);
+  }
+  public int getCounter() {
+    return counter;
+  }
+  public void incrementCounter() {
+    counter++;
+  }
+  public void resetCounter() {
+    counter = 0;
+  }
   public int getDieValue() {
     return dieValue;
   }
-
   public int getRoundScore() {
     return roundScore;
   }
-
-  public int getTotalScore() {
-    return totalScore;
+  public void updateRoundScore() {
+    roundScore += dieValue;
   }
-
-  // Roll the die
-  // If the player rolls a '1', reset the the round score
-  // Otherwise, add the roll to the round score
-  public void rollDie() {
-    dieValue = gen.nextInt(6) + 1;
-    if (dieValue == 1) {
-      resetRoundScore();
-    } else {
-      roundScore += dieValue;
-    }
-  }
-
   public void resetRoundScore() {
     roundScore = 0;
   }
-
-  // At the end of a round, add the round score to the player's total score
-  public void endRound() {
+  public int getTotalScore() {
+    return totalScore;
+  }
+  public void updateTotalScore() {
     totalScore += roundScore;
   }
-
-  public boolean playerWon() {
-    return totalScore >= 50;
+  public void rollDie() { // Roll the die
+    incrementCounter();
+    dieValue = gen.nextInt(6) + 1;
+    if (dieValue == 1) {  // If the player rolls a '1',
+      endRound();         // end the round
+    } else {
+      updateRoundScore(); // Otherwise, add the roll to the round score
+    }
   }
-
+  public void endRound() {
+    updateTotalScore();
+    resetRoundScore();
+    resetCounter();
+    togglePlayerTurn();
+  }
+  public boolean playerWon() {
+    return totalScore >= winningScore;
+  }
 }
