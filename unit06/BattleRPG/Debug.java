fb0fe653d17b633c29bbee973f41eb84f916f13f
Chase May <chaseloganmay@gmail.com>
2019-02-27 23:58:54 -0700

    Update comment to reflect new filename

--- unit06/BattleRPG/Debug.java
+++ unit06/BattleRPG/Debug.java
@@ -1,4 +1,4 @@
-// Implement `Display` if you would like to print an object's state for easy debugging
+// Implement `Debug` if you would like to print an object's state for easy debugging
 interface Debug {
   public void debug();
 }
