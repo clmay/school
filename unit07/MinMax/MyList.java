03d48b9d88445c2ff1643d05b859c1c4534189c4
Chase May <chaseloganmay@gmail.com>
2019-03-13 12:47:09 -0600

    Implement MyList.java

--- unit07/MinMax/MyList.java
+++ unit07/MinMax/MyList.java
@@ -1,18 +1,37 @@
 package MinMax;
 
+import java.util.Random;
+
 public class MyList implements MinMax {
+    private int[] numbers = new int[20];
+    private static Random generator = new Random();
 
-    @Override
+    public MyList() {
+        for (int item : numbers) {
+            item = generator.nextInt(100) + 1;
+        }
+    }
+
     public int getMin(int[] arr) {
-        return 0;
+        int min = arr[0];
+        for (int item : arr) {
+            if (item < min) {
+                min = num;
+            }
+        }
+        return min;
     }
 
-    @Override
     public int getMax(int[] arr) {
-        return 0;
+        int max = arr[0];
+        for (int item : arr) {
+            if (item > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 
-    @Override
     public String toString(int[] arr) {
         return null;
     }
