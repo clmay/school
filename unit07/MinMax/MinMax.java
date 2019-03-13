766e396a041751d40c8e49a41f98aa5184781c74
Chase May <chaseloganmay@gmail.com>
2019-03-13 12:25:04 -0600

    Add MinMax

---
+++ unit07/MinMax/MinMax.java
@@ -0,0 +1,7 @@
+package MinMax;
+
+public interface MinMax {
+    int getMin(int[] arr);
+    int getMax(int[] arr);
+    String toString(int[] arr);
+}
---
+++ unit07/MinMax/MyList.java
@@ -0,0 +1,20 @@
+package MinMax;
+
+public class MyList implements MinMax {
+
+    @Override
+    public int getMin(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int getMax(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public String toString(int[] arr) {
+        return null;
+    }
+
+}
