3fa6b2d78d0db6af427d8f9b0bf8b1fc00dbe832
Chase May <chaseloganmay@gmail.com>
2019-03-14 00:03:19 -0600

    Implement program loop

--- unit07/MinMax/MinMaxCLI.java
+++ unit07/MinMax/MinMaxCLI.java
@@ -1,5 +1,24 @@
 package MinMax;
 
+import java.util.Scanner;
+
 public class MinMaxCLI {
-    
+    private static Scanner scan = new Scanner(System.in);
+
+    public MinMaxCLI() {
+        String choice = "";
+        while (choice != "Q") {
+            MyList list = new MyList();
+            System.out.println("Values: " + list.toString());
+            System.out.println("Min: " + list.getMin());
+            System.out.println("Max: " + list.getMax());
+            System.out.println("Press Enter to view a new list, or Q to Quit.");
+            choice = scan.nextLine();
+        }
+    }
+
+    public static void main(String[] args) {
+        new MinMaxCLI();
+    }
+
 }
