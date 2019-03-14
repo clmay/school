3cff70ff48f543a79a9588ebb2bf734db13fcfe2
Chase May <chaseloganmay@gmail.com>
2019-03-14 12:17:29 -0600

    Format

--- assignments/07-assignment.md
+++ assignments/07-assignment.md
@@ -1,5 +1,5 @@
 1. List the names of instructors who have not used projects (PJ) as a basis for
-grading in their courses. Arrange by last name.
+   grading in their courses. Arrange by last name.
 
 ```
 LAST_NAME    FIRST_NAME
@@ -11,7 +11,7 @@ Willig       Irene
 ```
 
 2. Determine the number of students who received a below average grade on the
-final exam (FI) in section 86.
+   final exam (FI) in section 86.
 
 ```
 BELOW_AVERAGE
@@ -30,7 +30,7 @@ Brooklyn   NY
 ```
 
 4. Provide the student_id, name and final exam grade of the student(s) that
-received the lowest score on the final exam (FI) in course section 81.
+   received the lowest score on the final exam (FI) in course section 81.
 
 ```
 STUDENT_ID FIRST_NAME LAST_NAME      NUMERIC_GRADE
@@ -40,7 +40,7 @@ STUDENT_ID FIRST_NAME LAST_NAME      NUMERIC_GRADE
 ```
 
 5. List the student id and name of students who have taken the same course more
-than once. Arrange by last name then first name.
+   than once. Arrange by last name then first name.
 
 ```
 STUDENT_ID LAST_NAME    FIRST_NAME
@@ -52,7 +52,7 @@ STUDENT_ID LAST_NAME    FIRST_NAME
 ```
 
 6. List the first and last names of the students who have enrolled in the fewest
-Sophomore (200 level) classes. Order by last name of the student
+   Sophomore (200 level) classes. Order by last name of the student
 
 ```
 FIRST_NAME                LAST_NAME
@@ -64,7 +64,7 @@ Donna                     Walston
 ```
 
 7. List the course number and course description of the courses with the highest
-number of enrolled students. Arrange the list on course description.
+   number of enrolled students. Arrange the list on course description.
 
 ```
 COURSE_NO DESCRIPTION
@@ -73,7 +73,7 @@ COURSE_NO DESCRIPTION
 ```
 
 8. List the names of the students who are taking a course where at least one
-section of the course begins at 10:30 AM. Sort by last name.
+   section of the course begins at 10:30 AM. Sort by last name.
 
 ```
 FIRST_NAME                LAST_NAME
@@ -89,8 +89,8 @@ Salewa                    Zuckerberg
 ```
 
 9. List the first and last names of students are in trouble of failing section
-87 because they received a score on the final exam that was below the average of
-the rest of the class for that final exam. Order the list by last name
+   87 because they received a score on the final exam that was below the average of
+   the rest of the class for that final exam. Order the list by last name
 
 ```
 FIRST_NAME                LAST_NAME
@@ -100,10 +100,10 @@ Sean                      Pineda
 ```
 
 10. Provide an alphabetic list containing the full names of students (order by
-last name) and phone number who are the smartest students because they have
-taken at least one of the database courses. (Intro to SQL, Oracle Tools, PL/SQL
-Programming, Database Design, Database System Principles, DB Programming with
-Java). Your query must call the class by the description.
+    last name) and phone number who are the smartest students because they have
+    taken at least one of the database courses. (Intro to SQL, Oracle Tools, PL/SQL
+    Programming, Database Design, Database System Principles, DB Programming with
+    Java). Your query must call the class by the description.
 
 ```
 FIRST_NAME                LAST_NAME                 PHONE
--- assignments/07-assignment.sql
+++ assignments/07-assignment.sql
@@ -1,34 +1,52 @@
-/* 1. List the names of instructors who have not used projects (PJ) as a basis for
-grading in their courses. Arrange by last name. */
+/*
+    1. List the names of instructors who have not used projects (PJ) as a basis for
+    grading in their courses. Arrange by last name.
+*/
 
+/*
+    2. Determine the number of students who received a below average grade on the
+    final exam (FI) in section 86.
+*/
 
+/*
+    3. List the city and state that have the highest number of students enrolled.
+*/
 
-/* 2. Determine the number of students who received a below average grade on the
-final exam (FI) in section 86. */
+/*
+    4. Provide the student_id, name and final exam grade of the student(s) that
+    received the lowest score on the final exam (FI) in course section 81.
+*/
 
-/* 3. List the city and state that have the highest number of students enrolled. */
+/*
+    5. List the student id and name of students who have taken the same course more
+    than once. Arrange by last name then first name.
+*/
 
-/* 4. Provide the student_id, name and final exam grade of the student(s) that
-received the lowest score on the final exam (FI) in course section 81. */
+/*
+    6. List the first and last names of the students who have enrolled in the fewest
+    Sophomore (200 level) classes. Order by last name of the student
+*/
 
-/* 5. List the student id and name of students who have taken the same course more
-than once. Arrange by last name then first name. */
+/*
+    7. List the course number and course description of the courses with the highest
+    number of enrolled students. Arrange the list on course description.
+*/
 
-/* 6. List the first and last names of the students who have enrolled in the fewest
-Sophomore (200 level) classes. Order by last name of the student */
+/*
+    8. List the names of the students who are taking a course where at least one
+    section of the course begins at 10:30 AM. Sort by last name.
+*/
 
-/* 7. List the course number and course description of the courses with the highest
-number of enrolled students. Arrange the list on course description. */
+/*
+    9. List the first and last names of students are in trouble of failing section
+    87 because they received a score on the final exam that was below the average of
+    the rest of the class for that final exam. Order the list by last name
+*/
 
-/* 8. List the names of the students who are taking a course where at least one
-section of the course begins at 10:30 AM. Sort by last name. */
-
-/* 9. List the first and last names of students are in trouble of failing section
-87 because they received a score on the final exam that was below the average of
-the rest of the class for that final exam. Order the list by last name */
-
-/* 10. Provide an alphabetic list containing the full names of students (order by
-last name) and phone number who are the smartest students because they have
-taken at least one of the database courses. (Intro to SQL, Oracle Tools, PL/SQL
-Programming, Database Design, Database System Principles, DB Programming with
-Java). Your query must call the class by the description. */
+/*
+    10. Provide an alphabetic list containing the full names of students (order by
+    last name) and phone number who are the smartest students because they have
+    taken at least one of the database courses. (Intro to SQL, Oracle Tools, PL/SQL
+    Programming, Database Design, Database System Principles, DB Programming with
+    Java). Your query must call the class by the description.
+*/
