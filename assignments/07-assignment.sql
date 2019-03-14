c5049e3e9d461ab38530693716a598adb9898fd1
Chase May <chaseloganmay@gmail.com>
2019-03-14 12:44:23 -0600

    Formatting

--- assignments/07-assignment.sql
+++ assignments/07-assignment.sql
@@ -3,45 +3,63 @@
     grading in their courses. Arrange by last name.
 */
 
+
+
 /*
     2. Determine the number of students who received a below average grade on the
     final exam (FI) in section 86.
 */
 
+
+
 /*
     3. List the city and state that have the highest number of students enrolled.
 */
+
+
 
 /*
     4. Provide the student_id, name and final exam grade of the student(s) that
     received the lowest score on the final exam (FI) in course section 81.
 */
 
+
+
 /*
     5. List the student id and name of students who have taken the same course more
     than once. Arrange by last name then first name.
 */
+
+
 
 /*
     6. List the first and last names of the students who have enrolled in the fewest
     Sophomore (200 level) classes. Order by last name of the student
 */
 
+
+
 /*
     7. List the course number and course description of the courses with the highest
     number of enrolled students. Arrange the list on course description.
 */
+
+
 
 /*
     8. List the names of the students who are taking a course where at least one
     section of the course begins at 10:30 AM. Sort by last name.
 */
 
+
+
 /*
     9. List the first and last names of students are in trouble of failing section
     87 because they received a score on the final exam that was below the average of
     the rest of the class for that final exam. Order the list by last name
 */
+
+
 
 /*
     10. Provide an alphabetic list containing the full names of students (order by
