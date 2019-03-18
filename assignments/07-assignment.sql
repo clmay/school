7d0f846e6c1e70b02bc867003fbf7ba1ff757a0d
Chase May <chaseloganmay@gmail.com>
2019-03-18 10:55:47 -0600

    Finish problem 8

--- assignments/07-assignment.sql
+++ assignments/07-assignment.sql
@@ -8,7 +8,8 @@ SELECT
 FROM
     instructor i
 WHERE
-    i.instructor_id NOT IN (
+    i.instructor_id NOT IN
+    (
         SELECT DISTINCT
             i.instructor_id
         FROM
@@ -22,7 +23,6 @@ ORDER BY
     i.last_name;
 
 
-
 /* =====
     2. Determine the number of students who received a below average grade on the
     final exam (FI) in section 86.
@@ -33,7 +33,8 @@ SELECT
 FROM
     grade g
 WHERE
-    g.numeric_grade < (
+    g.numeric_grade < 
+    (
         SELECT
             AVG(g.numeric_grade)
         FROM
@@ -44,7 +45,6 @@ WHERE
     )
     AND g.section_id = 86
     AND g.grade_type_code = 'FI';
-
 
 
 /* =====
@@ -58,26 +58,27 @@ FROM
     zipcode   z
     INNER JOIN student   s ON s.zip = z.zip
 HAVING
-    COUNT(*) = (
+    COUNT(*) =
+    (
         SELECT
             MAX(num_students)
-        FROM (
-                SELECT
-                    z.city,
-                    z.state,
-                    COUNT(*) AS num_students
-                FROM
-                    zipcode   z
-                    INNER JOIN student   s ON s.zip = z.zip
-                GROUP BY
-                    z.city,
-                    z.state
-            )
+        FROM
+        (
+            SELECT
+                z.city,
+                z.state,
+                COUNT(*) AS num_students
+            FROM
+                zipcode   z
+                INNER JOIN student   s ON s.zip = z.zip
+            GROUP BY
+                z.city,
+                z.state
+        )
     )
 GROUP BY
     z.city,
     z.state;
-
 
 
 /* =====
@@ -94,19 +95,19 @@ FROM
     student s
     INNER JOIN grade g ON g.student_id = s.student_id
 WHERE
-    g.numeric_grade = (
-            SELECT
-                MIN(g.numeric_grade) AS lowest_grade
-            FROM
-                student s
-                INNER JOIN grade g ON g.student_id = s.student_id
-            WHERE
-                g.section_id = 81
-                AND g.grade_type_code = 'FI'
-        )
+    g.numeric_grade =
+    (
+        SELECT
+            MIN(g.numeric_grade) AS lowest_grade
+        FROM
+            student s
+            INNER JOIN grade g ON g.student_id = s.student_id
+        WHERE
+            g.section_id = 81
+            AND g.grade_type_code = 'FI'
+    )
     AND g.section_id = 81
     AND g.grade_type_code = 'FI';
-
 
 
 /* =====
@@ -115,52 +116,52 @@ WHERE
 ===== */
 
 SELECT
-    st.student_id,
-    st.last_name,
-    st.first_name
+    s.student_id,
+    s.last_name,
+    s.first_name
 FROM
-    student      st
-    INNER JOIN enrollment   e ON e.student_id = st.student_id
-    INNER JOIN section      sec ON sec.section_id = e.section_id
+    student      s
+    INNER JOIN enrollment e ON e.student_id = s.student_id
+    INNER JOIN section sect ON sect.section_id = e.section_id
 HAVING
     COUNT(*) >= 2
 GROUP BY
-    st.student_id,
-    st.first_name,
-    st.last_name,
-    sec.course_no
+    s.student_id,
+    s.first_name,
+    s.last_name,
+    sect.course_no
 ORDER BY
-    st.last_name,
-    st.first_name;
+    s.last_name,
+    s.first_name;
 
 /* ----- OR -----
 SELECT
     student_id,
     last_name,
     first_name
-FROM (
-        SELECT
-            st.student_id,
-            st.first_name,
-            st.last_name,
-            COUNT(*)
-        FROM
-            student st
-            INNER JOIN enrollment e ON e.student_id = st.student_id
-            INNER JOIN section sec ON sec.section_id = e.section_id
-        HAVING
-            COUNT(*) >= 2
-        GROUP BY
-            st.student_id,
-            st.first_name,
-            st.last_name,
-            sec.course_no
-    )
+FROM
+(
+    SELECT
+        s.student_id,
+        s.first_name,
+        s.last_name,
+        COUNT(*)
+    FROM
+        student s
+        INNER JOIN enrollment e ON e.student_id = s.student_id
+        INNER JOIN section sect ON sect.section_id = e.section_id
+    HAVING
+        COUNT(*) >= 2
+    GROUP BY
+        s.student_id,
+        s.first_name,
+        s.last_name,
+        sect.course_no
+)
 ORDER BY
     last_name,
     first_name;
 */
-
 
 
 /* =====
@@ -169,38 +170,119 @@ ORDER BY
 ===== */
 
 SELECT
-    s.student_id
+    s.first_name,
+    s.last_name,
+    COUNT(*) as min_soph_classes
 FROM
     student s
     INNER JOIN enrollment e ON e.student_id = s.student_id
-    INNER JOIN section sec ON sec.section_id = e.section_id
+    INNER JOIN section sect ON sect.section_id = e.section_id
+WHERE
+    sect.course_no BETWEEN 200 AND 299
+GROUP BY
+    s.first_name,
+    s.last_name
 HAVING
-    COUNT(*) = (
-                    SELECT
-                        MIN(count_soph_classes)
-                    FROM (
-                            SELECT
-                                COUNT(*) AS count_soph_classes
-                            FROM
-                                student s
-                                INNER JOIN enrollment e ON e.student_id = s.student_id
-                                INNER JOIN section sec ON sec.section_id = e.section_id
-                            WHERE
-                                sec.course_no BETWEEN 200 AND 299
-                            GROUP BY
-                                s.student_id
-                        )
-                );
+    COUNT(*) =
+    (
+        SELECT
+            MIN(num_soph_classes)
+        FROM
+        (
+            SELECT
+                s.student_id,
+                COUNT(*) as num_soph_classes
+            FROM
+                student s
+                INNER JOIN enrollment e ON e.student_id = s.student_id
+                INNER JOIN section sect ON sect.section_id = e.section_id
+            WHERE
+                sect.course_no BETWEEN 200 AND 299
+            GROUP BY
+                s.student_id
+            ORDER BY
+                s.student_id
+        )
+    )
+ORDER BY
+    s.last_name;
 
 /* =====
     7. List the course number and course description of the courses with the highest
     number of enrolled students. Arrange the list on course description.
 ===== */
 
+SELECT
+    c.course_no,
+    c.description
+FROM
+    course c
+    INNER JOIN section s ON s.course_no = c.course_no
+    INNER JOIN enrollment e ON e.section_id = s.section_id
+GROUP BY
+    c.course_no,
+    c.description
+HAVING
+    COUNT(*) = 
+        (
+            SELECT
+                MAX(num_enrolled)
+            FROM
+                (
+                    SELECT
+                        c.course_no,
+                        c.description,
+                        COUNT(*) as num_enrolled
+                    FROM
+                        course c
+                        INNER JOIN section s ON s.course_no = c.course_no
+                        INNER JOIN enrollment e ON e.section_id = s.section_id
+                    GROUP BY
+                        c.course_no,
+                        c.description
+                )
+        );
+
+
 /* =====
     8. List the names of the students who are taking a course where at least one
     section of the course begins at 10:30 AM. Sort by last name.
 ===== */
+
+
+SELECT
+    s.first_name,
+    s.last_name
+FROM
+    student s
+    INNER JOIN enrollment e ON e.student_id = s.student_id
+    INNER JOIN section sect ON sect.section_id = e.section_id
+    INNER JOIN course c ON c.course_no = sect.course_no
+WHERE
+    c.course_no IN
+        (
+            SELECT
+                c.course_no
+            FROM
+                course c
+                INNER JOIN section sect ON sect.course_no = c.course_no
+            WHERE
+                sect.section_id IN
+                (
+                    SELECT
+                        sect.section_id
+                    FROM
+                        section sect
+                    WHERE
+                        TO_CHAR(sect.start_date_time, 'HH:MI') = '10:30'
+                )
+        )
+GROUP BY
+    s.first_name,
+    s.last_name
+ORDER BY
+    s.last_name;
+
 
 /* =====
     9. List the first and last names of students are in trouble of failing section
