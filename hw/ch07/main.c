792cd3abc770557bfd70a791fa9244ca223f558d
Chase May <chaseloganmay@gmail.com>
2019-03-21 15:13:01 -0600

    Refactor main() to initialize ShoppingCart with user input

--- hw/ch07/main.c
+++ hw/ch07/main.c
@@ -4,30 +4,39 @@
 #include "ShoppingCart.h"
 #include "ItemToPurchase.h"
 
+#define CUST_NAME_LEN 50
+#define CURR_DATE_LEN 20
+
+void PrintMenu(ShoppingCart cart);
+
 int main(int argc, char const *argv[]) {
+    // Declare variables
+    char name[CUST_NAME_LEN];
+    char date[CURR_DATE_LEN];
     ShoppingCart cart;
+
+    // Take user input: customer name
+    printf("Enter Customer's Name:\n");
+    fgets(name, CUST_NAME_LEN, stdin);
+    name[strlen(name) - 1] = '\0';
+
+    // Take user input: current date
+    printf("Enter Today's Date:\n");
+    fgets(date, CURR_DATE_LEN, stdin);
+    date[strlen(date) - 1] = '\0';
+
     // Initialize ShoppingCart
-    strcpy(cart.customerName, "John Doe");
-    strcpy(cart.currentDate, "February 1, 2016");
+    strcpy(cart.customerName, name);
+    strcpy(cart.currentDate, date);
     cart.cartSize = 0;
 
-    ItemToPurchase item1;
-    // Initialize an ItemToPurchase
-    strcpy(item1.itemName, "Nike Romaleos");
-    strcpy(item1.itemDescription, "Some kind of shoes");
-    item1.itemQuantity = 2;
-    item1.itemPrice = 189;
-
-    PrintTotal(cart);
-
-    cart = AddItem(item1, cart);
-    PrintTotal(cart);
-    printf("\n");
-    printf("Total units in cart: %d\n", GetNumItemsInCart(cart));
-
-    cart = RemoveItem("Nike Romaleos", cart);
-    PrintTotal(cart);
-    printf("\n");
-
+    // Print initial information
+    printf("\nCustomer Name: %s\n", cart.customerName);
+    printf("Today's Date: %s\n", cart.currentDate);
+    
     return 0;
+}
+
+void PrintMenu(ShoppingCart cart) {
+    return;
 }
