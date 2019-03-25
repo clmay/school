f7ea93a555581c3e4d3010c45c580a99b6cf2df1
Chase May <chaseloganmay@gmail.com>
2019-03-25 12:40:27 -0600

    Fix name, typos

--- unit09/NumberList.java
+++ unit09/NumberList.java
@@ -3,18 +3,18 @@ import java.io.*;
 
 class NumberList {
     public static void main(String [] args) {
-       try{
+       try {
           int sum = 0;
           // 1. Declare a file to open the numbers.txt file
-          Files numberFile = new File("numbers.txt");
+          File numberFile = new File("numbers.txt");
           
           // 2. Create an instance of the scanner to scan the file you just created
-          Scanner read = new Scanner(numberFile);
+          Scanner fileRead = new Scanner(numberFile);
           
           // 3. Complete the while loop to continue reading so long as the file has more values
-          while (numberFile.hasNext()) {
+          while (fileRead.hasNext()) {
              // 4. Complete the sum statement below to store the next int in the file into num.
-             int num = read.nextLine();
+             int num = fileRead.nextInt();
              // This command prints out the number to make sure it is working. 
              System.out.println ("num: " + num);
              // This line of code sums up all the numbers.  
@@ -24,7 +24,7 @@ class NumberList {
           System.out.println("The Sum is: " + sum);
         
           // 5. Close the Scanner object
-          numberFile.close();
+          fileRead.close();
       }
       // If any other error occurred, display the message
        catch(Exception e) {
