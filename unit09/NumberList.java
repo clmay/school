a7479fb99e55bcda993b56f692df21afd423d700
Chase May <chaseloganmay@gmail.com>
2019-03-25 12:41:52 -0600

    Formatting

--- unit09/NumberList.java
+++ unit09/NumberList.java
@@ -2,33 +2,33 @@ import java.util.*;
 import java.io.*;
 
 class NumberList {
-    public static void main(String [] args) {
-       try {
-          int sum = 0;
-          // 1. Declare a file to open the numbers.txt file
-          File numberFile = new File("numbers.txt");
-          
-          // 2. Create an instance of the scanner to scan the file you just created
-          Scanner fileRead = new Scanner(numberFile);
-          
-          // 3. Complete the while loop to continue reading so long as the file has more values
-          while (fileRead.hasNext()) {
-             // 4. Complete the sum statement below to store the next int in the file into num.
-             int num = fileRead.nextInt();
-             // This command prints out the number to make sure it is working. 
-             System.out.println ("num: " + num);
-             // This line of code sums up all the numbers.  
-             sum += num;
-          } 
-          // This displays the sum        
-          System.out.println("The Sum is: " + sum);
-        
-          // 5. Close the Scanner object
-          fileRead.close();
+   public static void main(String [] args) {
+      try {
+         int sum = 0;
+         // 1. Declare a file to open the numbers.txt file
+         File numberFile = new File("numbers.txt");
+         
+         // 2. Create an instance of the scanner to scan the file you just created
+         Scanner fileRead = new Scanner(numberFile);
+         
+         // 3. Complete the while loop to continue reading so long as the file has more values
+         while (fileRead.hasNext()) {
+            // 4. Complete the sum statement below to store the next int in the file into num.
+            int num = fileRead.nextInt();
+            // This command prints out the number to make sure it is working. 
+            System.out.println ("num: " + num);
+            // This line of code sums up all the numbers.  
+            sum += num;
+         } 
+         // This displays the sum        
+         System.out.println("The Sum is: " + sum);
+      
+         // 5. Close the Scanner object
+         fileRead.close();
+   }
+   // If any other error occurred, display the message
+      catch(Exception e) {
+         System.out.println("Error: " + e.getMessage());
       }
-      // If any other error occurred, display the message
-       catch(Exception e) {
-          System.out.println("Error: " + e.getMessage());
-       }
-    }
- } 
+   }
+}
