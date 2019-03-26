c7462e71332850fa98401bc41a0245b67942e5a2
Chase May <chaseloganmay@gmail.com>
2019-03-26 08:58:48 -0600

    Review homework

---
+++ mod08/hw/ItemToPurchase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ItemToPurchase.h"
+
+void MakeItemBlank(ItemToPurchase* item) {
+    strcpy(item->itemName, "none");
+    strcpy(item->itemDescription, "none");
+    item->itemPrice = 0;
+    item->itemQuantity = 0;
+}
+
+void PrintItemCost(const ItemToPurchase* item) {
+    printf("%s %d @ $%d = $%d\n"
+            , item->itemName
+            , item->itemQuantity
+            , item->itemPrice
+            , item->itemPrice * item->itemQuantity
+        );
+}
+
+void PrintItemDescription(const ItemToPurchase item) {
+    printf("%s: %s\n", item->itemName, item->itemDescription);
+}
---
+++ mod08/hw/ItemToPurchase.h
@@ -0,0 +1,21 @@
+#ifndef ITEMTOPURCHASE__INC__
+#define ITEMTOPURCHASE__INC__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ITEM_NAME_LEN 50
+#define ITEM_DESC_LEN 100
+
+typedef struct ItemToPurchase {
+    char itemName[ITEM_NAME_LEN];
+    char itemDescription[ITEM_DESC_LEN];
+    int itemPrice;
+    int itemQuantity;
+} ItemToPurchase;
+
+void MakeItemBlank(ItemToPurchase* item);
+void PrintItemCost(const ItemToPurchase* item);
+void PrintItemDescription(const ItemToPurchase* item);
+
+#endif
