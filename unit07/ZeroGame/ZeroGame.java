baf8f5cae6fd858a64524daaf8ae715d1e9ef0cd
Chase May <chaseloganmay@gmail.com>
2019-03-26 12:01:32 -0600

    Reset ZeroGame progress

--- unit07/ZeroGame/ZeroGame.java
+++ unit07/ZeroGame/ZeroGame.java
@@ -3,57 +3,53 @@ package ZeroGame;
 import java.util.Random;
 
 public class ZeroGame {
-	private int numbers[] = new int[10];
-	private static Random generator = new Random();
+    private int numbers[];
+    private static Random generator = new Random();
 
-	public ZeroGame() {
-		// Instantiate the array of 10 values and fill them with the numbers 0 through 9
-		// Then mix the numbers up so they are out of order
-		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = i;
-		}
-		for (int i = 0; i < numbers.length; i++) {
-			int rnd1 = generator.nextInt(numbers.length) + 1;
-			int rnd2 = generator.nextInt(numbers.length) + 1;
-			swap(rnd1, rnd2);
-		}
-	}
+    public ZeroGame() {
+        // Instantiate the array of 10 values and fill them with the numbers 0 through 9
+        // Then mix the numbers up so they are out of order
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int rnd1 = generator.nextInt(numbers.length) + 1;
+            int rnd2 = generator.nextInt(numbers.length) + 1;
+            swap(rnd1, rnd2);
+        }
+    }
 
-	private void swap(int pos1, int pos2) {
-		// Swap the two positions in the list
-	}
+    private void swap(int pos1, int pos2) {
+        // Swap the two positions in the list
+    }
 
-	public int getZeroPos() {
-		// Return the position of the zero in the list
-		return -1;
-	}
+    public int getZeroPos() {
+        // Return the position of the zero in the list
+        return -1;
+    }
 
-	public void moveRight() {
-		// If the zero is all the way to the right, don't do anything
-		// Othewise: Swap the zero with the number to the right
-	}
+    public void moveRight() {
+        // If the zero is all the way to the right, don't do anything
+        // Othewise: Swap the zero with the number to the right
+    }
 
-	public void moveLeft() {
-		// If the zero is all the way to the left, don't do anything
-		// Otherwise: Swap the zero with the number to the left
-	}
+    public void moveLeft() {
+        // If the zero is all the way to the left, don't do anything
+        // Othewise: Swap the zero with the number to the left
+    }
 
-	public void flip() {
-		// If the zero is on an end, don't do anything
-		// Otherwise, swap the two numbers on either side of the zero
-	}
+    public void flip() {
+        // If the zero is on an end, don't do anything
+        // Otherwise, swap the two numbers on either side of the zero
+    }
 
-	public boolean inOrder() {
-		// Return true if the numbers are in order
-		return false;
-	}
+    public boolean inOrder() {
+        // Return true if the numbers are in order
+        return false;
+    }
 
-	public String toString() {
-		String str = "";
-		str += numbers[0];
-		for (int i = 1; i < numbers.length; i++) {
-			str += " " + numbers[i];
-		}
-		return str;
-	}
+    public String toString() {
+        // Return a String of all the numbers seporated with a space
+        return "";
+    }
 }
--- unit07/ZeroGame/ZeroGameDisplay.java
+++ unit07/ZeroGame/ZeroGameDisplay.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 public class ZeroGameDisplay extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JFrame window = new JFrame("Zero Game");
+    private JFrame window = new JFrame("Zero Game");
 	private ZeroGame game = new ZeroGame();
 	private JLabel lblDisplay = new JLabel("Values: ");
 	private JButton btnRight = new JButton(">>");
@@ -14,43 +14,36 @@ public class ZeroGameDisplay extends JPanel {
 
 	public ZeroGameDisplay() {
 		setLayout(null);
-
 		lblDisplay.setBounds(20, 70, 200, 20);
 		btnLeft.setBounds(20, 120, 100, 20);
 		btnFlip.setBounds(140, 120, 100, 20);
 		btnRight.setBounds(260, 120, 100, 20);
-
 		add(btnRight);
 		add(btnLeft);
 		add(btnFlip);
 		add(lblDisplay);
-
 		window.add(this);
 		window.setSize(500, 300);
 		window.setVisible(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		btnLeft.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				game.moveLeft();
 				updateGame();
 			}
 		});
-
 		btnFlip.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				game.flip();
 				updateGame();
 			}
 		});
-
 		btnRight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				game.moveRight();
 				updateGame();
 			}
 		});
-
 		game = new ZeroGame();
 		lblDisplay.setText(game.toString());
 	}
