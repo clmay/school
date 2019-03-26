af5b25051ff11db1183b64bea7b75b8551570409
Chase May <chaseloganmay@gmail.com>
2019-03-26 10:02:15 -0600

    Complete skeleton for PlaylistNode

---
+++ mod10/hw/PlaylistNode.c
@@ -0,0 +1,53 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  PlaylistNode.c
+ *
+ *    Description:  Playlist library functions
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:03:59 AM
+ *       Revision:  none
+ *          Usage:  #include "PlaylistNode.h"
+ *
+ *         Author:  Chase May (), chasemay@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "PlaylistNode.h"
+
+void CreatePlaylistNode(PlaylistNode* this_node,
+                        char idInit[],
+                        char songNameInit[],
+                        char artistNameInit[],
+                        int songLengthInit,
+                        PlaylistNode* nextNode) {
+    // 1) Set all string fields in thisNode to the input parameters (strcpy)
+    // 2) Set all integer fields in thisNode to the input parameterss
+    // 3) Set the nextNodePtr field to the nextNode
+
+}
+
+void InsertPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* nextNode) {
+    // Set the nextNodePtr field of thisNode to nextNode
+    PlaylistNode* tmp = NULL;
+    tmp = thisNode->nextNodePtr;
+    thisNode->nextNodePtr = nextNode;
+    nextNode->nextNodePtr = tmp;
+}
+
+void SetNextPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* nextNode) {
+    // Set thisNode->nextNodePtr to nextNode
+}
+
+PlaylistNode* GetNextPlaylistNode(PlaylistNode* thisNode) {
+    // Return thisNode->nextNodePtr (the next node in the list)
+}
+
+void PrintListNode(PlaylistNode* thisNode) {
+    // Print all the nodes in the list after a given node
+}
--- mod10/hw/PlaylistNode.h
+++ mod10/hw/PlaylistNode.h
@@ -1,9 +1,9 @@
 /*
  * =====================================================================================
  *
- *       Filename:  PlaylistNode.c
+ *       Filename:  PlaylistNode.h
  *
- *    Description:  Playlist library
+ *    Description:  Playlist library header
  *
  *        Version:  1.0
  *        Created:  02/28/2019 09:03:59 AM
@@ -29,7 +29,7 @@ typedef struct PlaylistNode_struct {
     char songName[MAX];
     char artistName[MAX];
     int songLength;
-    struct PlaylistNode_struct* next_node_ptr; // address of next node, NULL for last member
+    struct PlaylistNode_struct* nextNodePtr; // address of next node, NULL for last member
 } PlaylistNode;
 
 void CreatePlaylistNode(PlaylistNode* this_node,
@@ -39,6 +39,9 @@ void CreatePlaylistNode(PlaylistNode* this_node,
                         int songLengthInit,
                         PlaylistNode* nextNode);
 
-void InsertPlaylistNode(PlaylistNode* thisNode, PlaylistNode* nextNode);
+void InsertPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* nextNode);
+void SetNextPlaylistNodeAfter(PlaylistNode* thisNode, PlaylistNode* nextNode);
+PlaylistNode* GetNextPlaylistNode(PlaylistNode* thisNode);
+void PrintListNode(PlaylistNode* thisNode);
 
 #endif
