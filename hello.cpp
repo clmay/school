056de2fa4ace8b95a295ec7a38a863438f73300f
Chase May <chaseloganmay@gmail.com>
2019-04-02 09:05:07 -0600

    Add first C++ code

--- hello.cpp
+++ hello.cpp
@@ -8,16 +8,18 @@
  *        Version:  1.0
  *        Created:  02/28/2019 09:03:59 AM
  *       Revision:  none
- *       Compiler:  gcc template.c -o template.out [-lm]
+ *       Compiler:  g++ template.cpp -o template.out [-lm]
  *          Usage:  ./template.out
  *
  *         Author:  Chase May (), chasemay@mail.weber.edu
  *   Organization:  WSU
  *
- * =====================================================================================
+ * =======================================  ==============================================
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 // Constants & Globals
 
@@ -26,7 +28,12 @@
 // Main Function
 int main(int argc, char* argv[])
 {
-
+    int num;
+    // for output use `cout`
+    cout << "Hello, C++!" << endl;
+    count << "Enter a number: " << endl;
+    cin >> num;
+    cout << "You entered: " << num << endl;
     return 0;
 }
 // Function Definitions
--- template.cpp
+++ template.cpp
@@ -8,16 +8,16 @@
  *        Version:  1.0
  *        Created:  02/28/2019 09:03:59 AM
  *       Revision:  none
- *       Compiler:  gcc template.c -o template.out [-lm]
+ *       Compiler:  g++ template.cpp -o template.out [-lm]
  *          Usage:  ./template.out
  *
  *         Author:  Chase May (), chasemay@mail.weber.edu
  *   Organization:  WSU
  *
- * =====================================================================================
+ * =======================================  ==============================================
  */
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 // Constants & Globals
 
