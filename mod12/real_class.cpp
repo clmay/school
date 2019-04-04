ce30d59edfb005cf89842b7bd20730d158cdfdf5
Chase May <chaseloganmay@gmail.com>
2019-04-04 10:01:06 -0600

    Move Movie code into Movie.h and Movie.cpp

--- mod12/real_class.cpp
+++ mod12/real_class.cpp
@@ -1,52 +1,44 @@
 /*
  * =====================================================================================
  *
- *       Filename:  real_class.cpp
+ *       Filename:  first_real_class.cpp
  *
- *    Description:  First *real* class definition
+ *    Description:  Let's change the structure to a class
  *
  *        Version:  1.0
- *        Created:  04/04/2019 08:55:48
+ *        Created:  04/04/2019 08:55:04 AM
  *       Revision:  none
- *       Compiler:  g++ real_class.cpp -o real_class.out -lm
+ *       Compiler (C++):  g++ first_real_class.cpp -o first_real_class.out -lm
+ *          Usage:  ./first_real_class.out 
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  Chase May (), chasemay@mail.weber.edu
+ *   Organization:  WSU
  *
  * =====================================================================================
  */
-
 #include <iostream>
+#include <iomanip>  // more `cout` options
+#include <string>   // for strings
+#include <vector>   // for vectors
+#include "Movie.h"
 using namespace std;
 
-class Movie
-{
-    // Only the instance has access to `private` fields and methods
-    private:
-    	string title = "";
-	int year = 0;
-    // All code has access to `public` fields and methods
-    public:
-	// Setters
-	void set_title(string title);
-	void set_year(int year);
-};
-
+// Main Function
 int main(int argc, char* argv[])
 {
-    Movie movie; // Create an  instace of the class (an object of class `Movie`)
-    movie.set_title("Batman");
+    cout << "The Movie List Program" << endl << endl
+	<< "Enter a movie..." << endl << endl;
+
+    // Get movies from user
+    vector<Movie> movies;
+    char another = 'y';
+    // Loop for entries
+    while (tolower(another) == 'y')
+    {
+	cout << endl << "Enter another movie? [y|n]";
+	cin >> another;
+	cin.ignore(); // ignore anything else in the buffer
+    }
+
     return 0;
-}
-
-void Movie::set_title(string title)
-{
-    title = title;
-    return;
-}
-
-void Movie::set_year(int year)
-{
-    year = year;
-    return;
 }
