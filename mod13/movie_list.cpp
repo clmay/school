b8cddfd075fe230bc5af7415aa2c466acfc50fee
Chase May <chaseloganmay@gmail.com>
2019-04-09 10:11:22 -0600

    Add mod13

---
+++ mod13/movie_list.cpp
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  _movie.cpp
+ *
+ *    Description:  Save a list of movies to a file
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 08:40:08 AM
+ *       Revision:  none
+ *       Compiler (C++):  g++ next_movie.cpp Movie.cpp -o next_movie.out
+ *          Usage:  ./next_movie.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Movie.h"
+using namespace std;
+
+// Constants & Globals
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+// Function Definitions
