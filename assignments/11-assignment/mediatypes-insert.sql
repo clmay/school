42126cbb289d04200ba41c5a6b9dc5c77f1da1ca
Chase May <chaseloganmay@gmail.com>
2019-04-10 17:18:01 -0600

    Add insert statement files

---
+++ assignments/11-assignment/mediatypes-insert.sql
@@ -0,0 +1,7 @@
+INSERT ALL
+    INTO mediatype(mediatypeID, name) VALUES (1, 'MPEG audio file')
+    INTO mediatype(mediatypeID, name) VALUES (2, 'Protected AAC audio file')
+    INTO mediatype(mediatypeID, name) VALUES (3, 'Protected MPEG4 video file')
+    INTO mediatype(mediatypeID, name) VALUES (4, 'Purchased AAC audio file')
+    INTO mediatype(mediatypeID, name) VALUES (5, 'AAC audio file')
+SELECT 1 FROM DUAL;
