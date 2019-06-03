9cb24d55bbfcedfbfd23bab3ecdcdfb249b9e084
Chase May <chaseloganmay@gmail.com>
2019-06-02 20:32:46 -0600

    Finish homework

---
+++ cs2130/p03_mod_hashing.py
@@ -0,0 +1,33 @@
+def main():
+    s = input("Please enter a sentence: ").lower()
+    s = strip(s)
+    h = mod_hash(sentence_sum(s))
+    print(h)
+
+
+def strip(sentence):
+    result = ""
+    for c in sentence:
+        if c == " ":
+            result += c
+        elif c.isalpha():
+            result += c
+    return result
+
+
+def sentence_sum(sentence):
+    sum = 0
+    for c in sentence:
+        if c == " ":
+            sum += 31
+        else:
+            sum += ord(c) - 96
+    return sum
+
+
+def mod_hash(num):
+    return num % 31
+
+
+if __name__ == "__main__":
+    main()
