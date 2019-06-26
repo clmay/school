e449d00a2aff618128bd5e02f18f16988784161c
Chase May <chaseloganmay@gmail.com>
2019-06-25 22:17:49 -0600

    Loopify

--- cs2705-python/06_classroom_network.py
+++ cs2705-python/06_classroom_network.py
@@ -7,57 +7,46 @@ classrooms = list(ipaddress.ip_network(
     my_ip_network).subnets(prefixlen_diff=2))
 
 small_class_1 = list(ipaddress.ip_network(classrooms[2]).subnets())
-small_class_2 = list(ipaddress.ip_network(classrooms[2]).subnets())
+small_class_2 = list(ipaddress.ip_network(classrooms[3]).subnets())
 offices = list(ipaddress.ip_network(small_class_2[1]).subnets())
 
-print("\nRouting table:")
+print("Routing table:")
 print("==============")
+print()
 
-print("\Lab 1:\n")
-print("Network address: {}".format(offices[0].network_address))
-print("Broadcast address: {}".format(offices[0].broadcast_address))
-print("Number of hosts: {}".format(len(list(offices[0].hosts()))))
-print("Valid host range: {} - {}".format(
-    offices[0].network_address - 1, offices[0].broadcast_address - 1))
+for i in range(0, 2):
+    print("Lab " + str(i + 1) + ":\n ")
+    print("Network address: {}".format(offices[i].network_address))
+    print("Broadcast address: {}".format(offices[i].broadcast_address))
+    print("Number of hosts: {}".format(len(list(offices[i].hosts()))))
+    print("Valid host range: {} - {}".format(
+        offices[i].network_address + 1, offices[i].broadcast_address - 1))
+    print()
 
-print("\nLab 2:\n")
-print("Network address: {}".format(offices[1].network_address))
-print("Broadcast address: {}".format(offices[1].broadcast_address))
-print("Number of hosts: {}".format(len(list(offices[1].hosts()))))
-print("Valid host range: {} - {}".format(
-    offices[0].network_address - 1, offices[1].broadcast_address - 1))
+for i in range(3, 5):
+    print("Classroom " + str(i) + ":\n ")
+    print("Network address: {}".format(small_class_1[i - 3].network_address))
+    print("Broadcast address: {}".format(
+        small_class_1[i - 3].broadcast_address))
+    print("Number of hosts: {}".format(
+        len(list(small_class_1[i - 3].hosts()))))
+    print("Valid host range: {} - {}".format(
+        small_class_1[i - 3].network_address + 1, small_class_1[i - 3].broadcast_address - 1))
+    print()
 
-print("\nClassroom 3:\n")
-print("Network address: {}".format(small_class_1[0].network_address))
-print("Broadcast address: {}".format(small_class_1[0].broadcast_address))
-print("Number of hosts: {}".format(len(list(small_class_1[0].hosts()))))
-print("Valid host range: {} - {}".format(
-    small_class_1[0].network_address - 1, small_class_1[0].broadcast_address - 1))
-
-print("\nClassroom 4:\n")
-print("Network address: {}".format(small_class_1[1].network_address))
-print("Broadcast address: {}".format(small_class_1[1].broadcast_address))
-print("Number of hosts: {}".format(len(list(small_class_1[1].hosts()))))
-print("Valid host range: {} - {}".format(
-    small_class_1[0].network_address - 1, small_class_1[1].broadcast_address - 1))
-
-print("\nClassroom 5:\n")
+print("Classroom 5:\n ")
 print("Network address: {}".format(small_class_2[0].network_address))
 print("Broadcast address: {}".format(small_class_2[0].broadcast_address))
 print("Number of hosts: {}".format(len(list(small_class_2[0].hosts()))))
 print("Valid host range: {} - {}".format(
-    small_class_2[0].network_address - 1, small_class_2[0].broadcast_address - 1))
+    small_class_2[0].network_address + 1, small_class_2[0].broadcast_address - 1))
+print()
 
-print("\nClassroom 1:\n")
-print("Network address: {}".format(classrooms[0].network_address))
-print("Broadcast address: {}".format(classrooms[0].broadcast_address))
-print("Number of hosts: {}".format(len(list(classrooms[0].hosts()))))
-print("Valid host range: {} - {}".format(
-    classrooms[0].network_address - 1, classrooms[0].broadcast_address - 1))
-
-print("\nClassroom 2:\n")
-print("Network address: {}".format(classrooms[1].network_address))
-print("Broadcast address: {}".format(classrooms[1].broadcast_address))
-print("Number of hosts: {}".format(len(list(classrooms[1].hosts()))))
-print("Valid host range: {} - {}".format(
-    classrooms[0].network_address - 1, classrooms[1].broadcast_address - 1))
+for i in range(0, 2):
+    print("Classroom " + str(i + 1) + ":\n ")
+    print("Network address: {}".format(classrooms[i].network_address))
+    print("Broadcast address: {}".format(classrooms[i].broadcast_address))
+    print("Number of hosts: {}".format(len(list(classrooms[i].hosts()))))
+    print("Valid host range: {} - {}".format(
+        classrooms[i].network_address + 1, classrooms[i].broadcast_address - 1))
+    print()
