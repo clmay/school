5b30fc9932e6bb16ac1ed467c6a5cb1795bc8db1
Chase May <chaseloganmay@gmail.com>
2019-07-23 21:01:05 -0600

    Complete assignment 4

---
+++ cs1410-c++/04-assignment/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+using namespace std;
+
+enum class Semester
+{
+  FALL,
+  SPRING,
+  SUMMER,
+  NONE
+};
+
+class Course
+{
+private:
+  int crn;
+  string name;
+  int creditHours;
+  int year;
+  string semester;
+
+public:
+  // Constructors
+  // nullary
+  Course() : crn(0), name(""), creditHours(0), year(0), semester("") {}
+  // binary
+  Course(int crn, string name) : crn(crn), name(name), creditHours(0), year(0), semester("") {}
+  // 5-ary
+  Course(int crn, string name, int hours, int year, string semester) : crn(crn), name(name), creditHours(hours), year(year), semester(semester) {}
+
+  // Destructor
+  ~Course() {}
+
+  // Accessors
+  int getCrn() const
+  {
+    return crn;
+  }
+  const string &getName() const
+  {
+    return name;
+  }
+  int getCreditHours() const
+  {
+    return creditHours;
+  }
+  int getYear() const
+  {
+    return year;
+  }
+  const string &getSemester() const
+  // Returns a string reference, is also const
+  {
+    return semester;
+  }
+
+  // Mutators
+  void setCrn(int crn)
+  {
+    // Field name must be fully qualified
+    Course::crn = crn;
+  }
+  void setName(const string &name)
+  {
+    // Field name must be fully qualified
+    Course::name = name;
+  }
+  void setCreditHours(int hours)
+  {
+    creditHours = hours;
+  }
+  void setYear(int year)
+  {
+    // Field name must be fully qualified
+    Course::year = year;
+  }
+  void setSemester(const string &semester)
+  {
+    // Field name must be fully qualified
+    Course::semester = semester;
+  }
+
+  // Auxiliary methods
+  string str() const
+  {
+    // Need to return a reference to a string, so instead of assigning directly
+    // to a string variable, we'll use new and use indirection.
+    string *output = new string;
+    stringstream ss;
+    ss << setw(5) << getCrn() << setw(35) << getName() << setw(9)
+       << getCreditHours() << setw(10) << getSemester() << setw(6) << getYear()
+       << endl;
+    *output = ss.str();
+    // We've got a pointer to the string, so we need to dereference it to return
+    // the string's value
+    return *output;
+  }
+};
+
+int main(int argc, char const *argv[])
+{
+  Course *c1 = new Course();
+  Course *c2 = new Course(11235, "Recursive Integer Sequences");
+  Course *c3 = new Course(11121, "Advanced Integer Sequences", 4, 2020, "Summer");
+
+  c1->setCrn(12345);
+  c1->setName("Introduction to Integer Sequences");
+  c1->setCreditHours(4);
+  c1->setYear(2019);
+  c1->setSemester("Fall");
+
+  c2->setCreditHours(4);
+  c2->setYear(2020);
+  c2->setSemester("Spring");
+
+  cout << endl;
+  cout << setw(5) << "CRN" << setw(35) << "Name" << setw(9) << "Credits"
+       << setw(10) << "Semester" << setw(6) << "Year" << endl;
+  cout << setw(5) << "-----------------------------------------------------------------"
+       << endl;
+  cout << c1->str();
+  cout << c2->str();
+  cout << c3->str();
+  cout << endl;
+
+  return 0;
+}
