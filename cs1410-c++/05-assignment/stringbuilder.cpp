22d847a342e8e4f4d4b0b4889c00cdd135ee325a
Chase May <chaseloganmay@gmail.com>
2019-07-30 17:15:30 -0600

    Complete assignment 5

---
+++ cs1410-c++/05-assignment/stringbuilder.cpp
@@ -0,0 +1,56 @@
+#include "stringbuilder.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// constructor: initializes `content` to an empty string
+StringBuilder::StringBuilder()
+{
+    this->content = "";
+}
+// constructor: initializes `content` to `str`
+StringBuilder::StringBuilder(const string& str)
+{
+    this->content = str;
+}
+// destructor
+StringBuilder::~StringBuilder() {}
+// a method I chose to add for convenience
+void StringBuilder::append(string str)
+{
+    this->content += str;
+}
+// returns the contents of a `StringBuilder` object
+const string& StringBuilder::str() const
+{
+    return this->content;
+}
+// duplicates the content `n` times within the `StringBuilder`
+void StringBuilder::operator*(unsigned int n)
+{
+    string original = this->content;
+    for (int i = 0; i < n; i++) {
+        this->content += original;
+    }
+}
+// compares whether the contents of two `StringBuilders` is equal
+bool StringBuilder::operator==(StringBuilder& sb)
+{
+    return this->content == sb.content;
+}
+// compares whether the contents of two `StringBuilders` are different
+bool StringBuilder::operator!=(StringBuilder& sb)
+{
+    return !(this->content == sb.content);
+}
+// allows you to store a `string` value into a `StringBuilder`
+void operator>>(string str, StringBuilder& sb)
+{
+    sb.content = str;
+}
+// allows you to stream a `StringBuilder` into an output stream
+ostream& operator<<(ostream& out, StringBuilder& sb)
+{
+    out << sb.content;
+    return out;
+}
---
+++ cs1410-c++/05-assignment/stringbuilder.h
@@ -0,0 +1,27 @@
+#ifndef STRINGBUILDER_H
+#define STRINGBUILDER_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StringBuilder {
+private:
+    string content;
+
+public:
+    StringBuilder();
+    StringBuilder(const string& str);
+    ~StringBuilder();
+
+    void append(string str);
+    const string& str() const;
+    void operator*(unsigned int n);
+    bool operator==(StringBuilder& sb);
+    bool operator!=(StringBuilder& sb);
+
+    friend void operator>>(string str, StringBuilder& sb);
+    friend ostream& operator<<(ostream& out, StringBuilder& sb);
+};
+
+#endif
