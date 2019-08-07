17bbd27478742cf73c968b2dda553a80f9328c9e
Chase May <chaseloganmay@gmail.com>
2019-08-06 22:53:34 -0600

    More line processing functions and comments

--- cs1410-c++/06-assignment/main.cpp
+++ cs1410-c++/06-assignment/main.cpp
@@ -24,22 +24,38 @@ int main(int argc, char const* argv[]) {
     int index_spaces_1 = line.find("  ");
 
     // get the index where the second set of spaces begins
+    // this is always at least four characters after the first set of spaces
+    // (since every number after the first set of spaces is also two characters
+    // wide)
     // second argument to `find()` is starting index
-    int index_spaces_2 = line.find("  ", index_spaces_1 + 1);
+    int index_spaces_2 = line.find("  ", index_spaces_1 + 4);
 
     // the groups of spaces are always at least 2 spaces wide
-    // so the first number begins 2 characters after the first set of spaces
-    string num_1 = line.substr(index_spaces_1 + 2, index_spaces_2 + 2);
-    // and the second number begins 2 characters after the second set of spaces
-    string num_2 = line.substr(index_spaces_2 + 2);
+    // the first number begins two characters after the first set of spaces and
+    // continues for two characters
+    string num_1 = line.substr(index_spaces_1 + 2, 2);
+    // the second number begins two characters after the second set of spaces
+    // and continues for two characters
+    string num_2 = line.substr(index_spaces_2 + 2, 2);
+
+    // sometimes the number is a single digit aligned to the right of the
+    // column, so we need to account for (and remove) the extra space in that
+    // case
+    if (num_1.substr(0, 1) == " ") {
+      num_1 = num_1.substr(1);
+    }
+    if (num_2.substr(0, 1) == " ") {
+      num_2 = num_2.substr(1);
+    }
 
     // assignment requires printing each line to stdout
     cout << line << endl;
 
     // (these are here for testing)
-    cout << op << endl;
-    cout << num_1 << endl;
-    cout << num_2 << endl;
+    cout << "Operator: " << op << endl;
+    cout << "Number 1: " << num_1 << endl;
+    cout << "Number 2: " << num_2 << endl;
+    cout << endl;
 
     // assignment requires outputting each line to the file, too
     out_f << line << endl;
