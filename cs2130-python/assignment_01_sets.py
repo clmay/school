dd52a86debfd3a104213644ffbc6e9a1bb127077
Chase May <chaseloganmay@gmail.com>
2020-01-19 14:15:49 -0700

    Refactor in a small way

--- cs2130-python/assignment_01_sets.py
+++ cs2130-python/assignment_01_sets.py
@@ -1,8 +1,4 @@
-a = {1, 3, 5, 6, 8}
-b = {2, 3, 4, 7, 9}
-
-
-def set_union(p, q):
+def union(p, q):
     res = set()
     for elem in p:
         res.add(elem)
@@ -11,7 +7,7 @@ def set_union(p, q):
     return res
 
 
-def set_intersection(p, q):
+def intersection(p, q):
     res = set()
     for elem_1 in p:
         for elem_2 in q:
@@ -20,7 +16,7 @@ def set_intersection(p, q):
     return res
 
 
-def set_complement(p, q):
+def relative_complement(p, q):
     res = set(p)
     for elem_1 in p:
         for elem_2 in q:
@@ -29,13 +25,16 @@ def set_complement(p, q):
     return res
 
 
-union = set_union(a, b)
-inter = set_intersection(a, b)
-rcomp = set_complement(a, b)
+a = {1, 3, 5, 6, 8}
+b = {2, 3, 4, 7, 9}
+
+a_u_b = union(a, b)
+a_n_b = intersection(a, b)
+a_c_b = relative_complement(a, b)
 
 print("A is " + str(a))
 print("B is " + str(b) + "\n")
 
-print("A u B is " + str(union))
-print("A n B is " + str(inter))
-print("A - B is " + str(rcomp))
+print("A u B is " + str(a_u_b))
+print("A n B is " + str(a_n_b))
+print("A - B is " + str(a_c_b))
