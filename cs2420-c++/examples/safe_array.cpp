90f7d45131aa79ae70d41397075ac70079a6cc2d
Chase May <chaseloganmay@gmail.com>
2020-01-22 21:17:08 -0700

    Reorganize

---
+++ cs2420-c++/examples/safe_array.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class SafeArray {
+public:
+  SafeArray(const unsigned int capacity);
+  ~SafeArray();
+  void item(const unsigned int index, const int value);
+  int get(const unsigned int index) const;
+
+private:
+  int* arr{ nullptr };
+  unsigned int capacity{ 0 };
+};
+
+SafeArray::SafeArray(const unsigned int capacity) {
+  arr = new int[capacity];
+  this->capacity = capacity;
+}
+
+SafeArray::~SafeArray() {
+  delete[] arr;
+}
+
+void SafeArray::item(const unsigned int index, const int value) {
+  if (index >= capacity) {
+    cout << "Index is out of bounds" << endl;
+  } else {
+    arr[index] = value;
+  }
+}
+
+int SafeArray::get(const unsigned int index) const {
+  return arr[index];
+}
+
+int main() {
+  SafeArray myObject(10);
+  myObject.item(1, 1);
+  myObject.item(2, 8);
+  myObject.item(3, 27);
+  myObject.item(4, 64);
+  myObject.item(10, 1000);
+  cout << myObject.get(4) << end cin.get();
+  return 0;
+}
